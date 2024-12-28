212575bca20719d05278c534aa5cb08ea8868c00
DenysLes <157131711+DenysLes@users.noreply.github.com>
2024-12-28 22:14:43 +0200

    add files

--- js/task-01.js
+++ js/task-01.js
@@ -1,52 +1,6 @@
-const itemsEl = document.querySelectorAll('.item');
-console.log(itemsEl);
-
-itemsEl.forEach(function (elem) {
-    console.log(`Titles : ${elem.length}`, `Titles : 3`, `Animals,Products,Technology`);
+const categoriesWithItems = document.querySelectorAll('li.item')
+categoriesWithItems.forEach(element => {
+    console.log(`Category: ${element.firstElementChild.textContent}`);
+    console.log(`Elements: ${element.lastElementChild.children.length}`);
 });
 
-const titleEl = document.querySelector('h2');
-console.log(titleEl);
-console.log(titleEl.textContent);
-
-
-const subList = document.querySelectorAll('ul');
-console.log(subList);
-
-const categories = document.querySelector('#categories');
-console.log(categories);
-
-subList.forEach(function (elem) {
-    console.log(elem);
-});
-
-const categoriesChildren = categories.childNodes;
-console.log(categoriesChildren);
-
-
-const itemELChildren = categories.childNodes;
-console.log(itemELChildren);
-
-const firstItemChildEl = categories.firstElementChild;
-console.log(firstItemChildEl);
-
-const lastItemChildEl = categories.lastElementChild;
-console.log(lastItemChildEl);
-
-const itemEl = document.querySelector('.item');
-console.log(itemEl);
-
-const titleAnimalsEl = itemEl.firstElementChild;
-console.log(titleAnimalsEl);
-
-const listAnimalsEl = titleAnimalsEl.nextElementSibling;
-console.log(listAnimalsEl);
-
-const itemAnimalListEL = listAnimalsEl.children;
-console.log(itemAnimalListEL);
-
-const animalsLiEl = document.querySelectorAll('li')
-
-animalsLiEl.forEach(function (elem) {
-        console.log(elem);
-    })
