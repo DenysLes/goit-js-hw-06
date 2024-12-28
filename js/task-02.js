10a21cc4cef0dce6941ee9832658fbff4a242145
DenysLes <157131711+DenysLes@users.noreply.github.com>
2024-12-28 22:36:06 +0200

    Update task-02.js

--- js/task-02.js
+++ js/task-02.js
@@ -6,14 +6,3 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
-
-//const itemsEl = <Створить>.  document.createElement()
-
-
-
-
-
-// Створить окремий елемент <>. Обов'язково використовуй метод document.createElement().
-//</>Додасть назву інгредієнта як його текстовий вміст.
-//Додасть елементу клас item.
-//Після чого, вставить усі <li> за одну операцію у список ul#ingredients.</li>
