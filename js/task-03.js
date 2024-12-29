cf1214dac0624ae257f03a890026ffe644022361
DenysLes <157131711+DenysLes@users.noreply.github.com>
2024-12-29 23:32:44 +0200

    update goit-js-hw-06 task 3. DONE

--- js/task-03.js
+++ js/task-03.js
@@ -16,24 +16,12 @@ const images = [
 const list = document.querySelector('.gallery');
 console.log(list);
 
-const markup = images.map((image) => `<li class="list-item"><img class='list-img' scr='${image.url}' alt='${image.alt}' width= 360 height=240 />
+const markup = images.map((image) => `<li class="list-item"><img class='list-img' src='${image.url}' alt='${image.alt}' width="360" height="240" />
   </li>`).join("");
 console.log(markup);
 
 const img = document.createElement('img');
 console.log(img);
-
-// img.setAttribute("scr",'https://images.pexels.com/photos/140134/pexels-photo-140134.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260');
-// img.setAttribute("scr",'https://images.pexels.com/photos/213399/pexels-photo-213399.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260');
-// img.setAttribute('https://images.pexels.com/photos/219943/pexels-photo-219943.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260');
-// img.setAttribute("alt", 'White and Black Long Fur Cat');
-
-
-
-console.log(img.attributes); 
-
-console.log(img.hasAttribute("src")); 
-
 
 list.insertAdjacentHTML("beforeend", markup);
 console.log(list);
@@ -42,12 +30,22 @@ console.log(list);
 
 
 
+// console.log(img.attributes); 
+
+// console.log(img.hasAttribute("src")); 
+
+
+
+
+
+
+
+
 // const refs = {
 //   ul: document.querySelector(".gallery")
 // };
-
 // const makeGalleryMarkup = ({ url, alt }) =>
-//   `<li class ='list gallery__list'><img scr="${url}" alt ="${alt}</li>`;
+//   `<li class ="list gallery__list"><img src="${url}" alt ="${alt}"></li>`;
 // console.log(makeGalleryMarkup);
 
 // const makeGallery = images.map(makeGalleryMarkup).join("");
