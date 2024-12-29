19a3c55e8901526a4d2b7165033cfac3558c157c
DenysLes <157131711+DenysLes@users.noreply.github.com>
2024-12-29 22:08:42 +0200

    update file

--- js/task-03.js
+++ js/task-03.js
@@ -13,30 +13,45 @@ const images = [
   },
 ];
 
-//insertAdjacentHTML()
 const list = document.querySelector('.gallery');
 console.log(list);
-// const newGallery = [
-//   {
-//     url: 'https://images.pexels.com/photos/140134/pexels-photo-140134.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
-//     alt: 'White and Black Long Fur Cat',
-//   },
-//   {
-//     url: 'https://images.pexels.com/photos/213399/pexels-photo-213399.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
-//     alt: 'Orange and White Koi Fish Near Yellow Koi Fish',
-//   },
-//   {
-//     url: 'https://images.pexels.com/photos/219943/pexels-photo-219943.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
-//     alt: 'Group of Horses Running',
-//   },
-// ];
-// console.log(newGallery);
-const markup = images
-  .map((itemOfGallery) => `<li class="list-item new">${itemOfGallery}</li>`)
-  .join("");
+
+const markup = images.map((image) => `<li class="list-item"><img class='list-img' scr='${image.url}' alt='${image.alt}' width= 360 height=240 />
+  </li>`).join("");
 console.log(markup);
+
 const img = document.createElement('img');
 console.log(img);
 
+// img.setAttribute("scr",'https://images.pexels.com/photos/140134/pexels-photo-140134.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260');
+// img.setAttribute("scr",'https://images.pexels.com/photos/213399/pexels-photo-213399.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260');
+// img.setAttribute('https://images.pexels.com/photos/219943/pexels-photo-219943.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260');
+
+
+
+img.setAttribute("alt", 'White and Black Long Fur Cat');
+console.log(img.attributes); 
+console.log(img);
+console.log(img.hasAttribute("src")); 
+
+
 list.insertAdjacentHTML("beforeend", markup);
 console.log(list);
+
+
+
+
+
+// const refs = {
+//   ul: document.querySelector(".gallery")
+// };
+
+// const makeGalleryMarkup = ({ url, alt }) =>
+//   `<li class ='list gallery__list'><img scr="${url}" alt ="${alt}</li>`;
+// console.log(makeGalleryMarkup);
+
+// const makeGallery = images.map(makeGalleryMarkup).join("");
+// console.log(makeGallery);
+
+// refs.ul.insertAdjacentHTML('beforeend', makeGallery);
+// console.log(refs);
