f2b2bb0c1f3925c65c2063c9e9bb95fa2f033a08
DenysLes <157131711+DenysLes@users.noreply.github.com>
2025-01-07 20:47:29 +0200

    Update task-09.js

--- js/task-09.js
+++ js/task-09.js
@@ -3,3 +3,15 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+const body = document.body;
+
+const spanColor = document.querySelector('.color');
+
+const changeColorBtn = document.querySelector('.change-color');
+
+
+function handleTargetBtnClick() {
+  spanColor.textContent = body.style.backgroundColor = getRandomHexColor()
+}
+
+changeColorBtn.addEventListener( 'click',handleTargetBtnClick )
