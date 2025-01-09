a86d2e246c418c42b1a4d5792476cdcd94941a2e
DenysLes <157131711+DenysLes@users.noreply.github.com>
2025-01-09 19:45:04 +0200

    Update task-10.js

--- js/task-10.js
+++ js/task-10.js
@@ -1,5 +1,24 @@
-function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215)
-    .toString(16)
-    .padStart(6, 0)}`;
+// function getRandomHexColor() {
+//   return `#${Math.floor(Math.random() * 16777215)
+//     .toString(16)
+//     .padStart(6, 0)}`;
+// }
+const divControls = document.querySelector('#controls')
+console.log(divControls);
+const inputNumber = document.querySelector('[type="number"]');
+console.log(inputNumber);
+const btnCreate = document.querySelector('button[data-create]');
+const btnDestroy = document.querySelector('button[data-destroy]');
+const divBoxesEl = document.getElementById('boxes')
+console.log(divBoxesEl);
+
+
+function createBoxes(amount) {
+  divBoxesEl = []
+  divBoxesEl.push.amount.value
 }
+
+
+
+
+
